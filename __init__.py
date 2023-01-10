16534eaff55be41dbec6d09b92d660da6d2db0bd
JonLim94 <jonathan.cllim@gmail.com>
2023-01-10 15:23:36 +0800

    SRBP Workshop 1.3.2

--- __init__.py
+++ __init__.py
@@ -5,30 +5,27 @@ class EasyShopping(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
+
+# SRBP Workshop 1.3.2 Adapt Intent Parser
 #    @intent_handler(IntentBuilder('FAQ.Adapt.intent').require('FAQ').require('ESA').build())
 #    def handle_shopping_easy(self, message):
 #        self.speak_dialog('shopping.easy')
-	
+
+
+
+# SRBP Workshop 1.3.2 Padatious Intent Parser	
     @intent_handler('FAQ.Adapt.intent')
     def handle_FAQ_Adapt(self, message):
     	self.speak('Hello ESA!')
  
-    
     	    	
-    @intent_handler('is.there.any.goods.intent')
-    def handle_is_there_any_goods(self, message):
+    @intent_handler('FAQ.Padatious.intent')
+    def handle_FAQ_Padatious(self, message):
         category_label = message.data.get('cat')
-        str = 'yes, I find ' +  category_label + ' is a skill which analyzes the photo taken by the user and then replies with the detected goods in the photos.'
+        str = category_label + ' is a skill which analyzes the photo taken by the user and then replies with the detected goods in the photos.'
         self.speak(str)
      	
-
-    @intent_handler('FAQ.Padatious.intent')
-    def handle_shopping_easy_padatious(self, message):
-    	ESA_label = message.data.get('cat_ESA')
-    	reply = str(ESA_label) + " is a skill which analyzes the photo taken by the user and then replies with the detected goods in the photos."
-    	self.speak(reply)
    
-
 def create_skill():
     return EasyShopping()
 
