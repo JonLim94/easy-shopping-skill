ebbf18432402fdbd9fb41a689dfcbc72c21976ab
JonLim94 <jonathan.cllim@gmail.com>
2023-01-12 20:51:27 +0800

    updated ws 2.4.1

--- cvAPI/util.py
+++ cvAPI/util.py
@@ -3,7 +3,7 @@ from requests import Session
 import io
 import base64
 
-API_KEY = 'xxxxxxxx'
+API_KEY = 'xxxxxxxxx'
 
 def encode_image_from_file(file_name):
     with io.open(file_name, 'rb') as image_file:
